b2aa946b41f64e6d29aabe8accb09d4f43a2b869
Eric Roos <eric@bookshout.com>
2016-03-23 15:53:32 +0000

    enforce dates

--- lib/hmac_shable.rb
+++ lib/hmac_shable.rb
@@ -15,7 +15,8 @@ module BookshoutHmacShaAuth::HmacShable
     timestamp   = request.headers["X-Bs-Timestamp"]  || ""
     param_list  = request.headers["X-Bs-Param-List"] || ""
     signature   = request.headers["X-Bs-Signature"]  || ""
-
+   
+    datetime = DateTime.parse(timestamp)
     Rails.logger.debug "Timestamp: #{timestamp}"
     Rails.logger.debug "ParamList: #{param_list}"
     Rails.logger.debug "Content-Type: #{request.headers["Content-Type"]}"
@@ -33,7 +34,7 @@ module BookshoutHmacShaAuth::HmacShable
     #if user_id && timestamp && param_list && signature &&  computed_signature == signature
     successfull_attempt = signature == computed_signature
     Rails.logger.debug "Access: #{successfull_attempt}"
-    if !successfull_attempt
+    if (datetime < DateTime.now-1.minute) && !successfull_attempt
       render(json: {:message => "Invalid auth credentials."}, :status => 401 )
     end 
   end 
