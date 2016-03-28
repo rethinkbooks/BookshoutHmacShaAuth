1a6d63e6f476623f1fa776b41ea7806e8924ff39
Eric Roos <eric@bookshout.com>
2016-03-28 14:15:43 +0000

    allow for local usage

--- lib/hmac_sha_generator.rb
+++ lib/hmac_sha_generator.rb
@@ -1,7 +1,11 @@
 class BookshoutHmacShaAuth::HmacShaGenerator
   def self.build_signature timestamp_str,params_string
     #key = "bookshout_key"
-    env_key =YAML.load_file("#{Rails.root}/config/hmac_sha_envs.yml")["hmac_sha_env_key"]
+    begin
+      env_key =YAML.load_file("#{Rails.root}/config/hmac_sha_envs.yml")["hmac_sha_env_key"]
+    rescue
+      env_key = "BOOKSHOUT_HMAC_SHA_LOCAL_KEY"
+    end
     key = ENV[env_key]
     data = timestamp_str + params_string 
     digest = OpenSSL::Digest.new('sha1')
