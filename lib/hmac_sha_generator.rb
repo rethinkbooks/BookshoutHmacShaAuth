42909af4c62309fd6aa7f357f007493a33d09161
Sean Eshbaugh <sean@bookshout.com>
2016-03-29 19:19:12 +0000

    Strip before encoding.

--- lib/hmac_sha_generator.rb
+++ lib/hmac_sha_generator.rb
@@ -1,5 +1,5 @@
 class BookshoutHmacShaAuth::HmacShaGenerator
-  def self.build_signature timestamp_str,params_string
+  def self.build_signature(timestamp_str, params_string)
     #key = "bookshout_key"
     begin
       env_key =YAML.load_file("#{Rails.root}/config/hmac_sha_envs.yml")["hmac_sha_env_key"]
@@ -7,9 +7,9 @@ class BookshoutHmacShaAuth::HmacShaGenerator
       env_key = "BOOKSHOUT_HMAC_SHA_LOCAL_KEY"
     end
     key = ENV[env_key]
-    data = timestamp_str + params_string 
+    data = timestamp_str + params_string
     digest = OpenSSL::Digest.new('sha1')
     hmac = OpenSSL::HMAC.digest(digest, key, data)
     Base64.encode64(hmac)
-  end 
+  end
 end
--- lib/hmac_shable.rb
+++ lib/hmac_shable.rb
@@ -9,24 +9,24 @@ module BookshoutHmacShaAuth::HmacShable
     #user_id     = params[:user_id]
 
     # auth params
-    
+
 
     Rails.logger.debug "Handling Auth for headers #{request.headers}"
     timestamp   = request.headers["X-Bs-Timestamp"]
     param_list  = request.headers["X-Bs-Param-List"]
     signature   = request.headers["X-Bs-Signature"]
-   
+
     datetime = DateTime.parse(timestamp)
     Rails.logger.debug "Timestamp: #{datetime}"
     Rails.logger.debug "ParamList: #{param_list}"
     Rails.logger.debug "Content-Type: #{request.headers["Content-Type"]}"
     param_str   = ""
     param_list.split(",").each do |param|
-      param_str << (params[param.to_sym].to_s || "") 
-    end 
+      param_str << (params[param.to_sym].to_s || "")
+    end
     Rails.logger.debug "Param str: #{param_str}"
     computed_signature = BookshoutHmacShaAuth::HmacShaGenerator.build_signature timestamp,param_str
-    computed_signature = URI::encode(computed_signature)
+    computed_signature = URI::encode(computed_signature.strip)
     Rails.logger.debug "#{signature}|"
     Rails.logger.debug "#{computed_signature}|"
     Rails.logger.debug params
@@ -42,9 +42,8 @@ module BookshoutHmacShaAuth::HmacShable
 
     if !successfull_attempt
       render(json: {:message => "Invalid auth credentials."}, :status => 401 )
-    end 
-  end 
+    end
+  end
 
 
 end
-
