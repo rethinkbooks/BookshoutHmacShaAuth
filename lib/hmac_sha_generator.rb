64677760dc6da3580ed4c6d351604e6717a0558b
Sean Eshbaugh <seaneshbaugh@gmail.com>
2017-02-01 23:37:30 -0600

    Style/ClassAndModuleChildren: Use nested module/class definitions instead of compact style.

--- lib/hmac_sha_generator.rb
+++ lib/hmac_sha_generator.rb
@@ -1,13 +1,15 @@
-class BookshoutHmacShaAuth::HmacShaGenerator
-  def self.build_signature(timestamp, params, application = 'www')
-    key = ENV["BOOKSHOUT_#{application.upcase}_HMAC_KEY"]
+module BookshoutHmacShaAuth
+  class HmacShaGenerator
+    def self.build_signature(timestamp, params, application = 'www')
+      digest = OpenSSL::Digest.new('sha1')
 
-    digest = OpenSSL::Digest.new('sha1')
+      key = ENV["BOOKSHOUT_#{application.upcase}_HMAC_KEY"]
 
-    data = timestamp + params
+      data = timestamp + params
 
-    hmac = OpenSSL::HMAC.digest(digest, key, data)
+      hmac = OpenSSL::HMAC.digest(digest, key, data)
 
-    Base64.encode64(hmac).strip
+      Base64.encode64(hmac).strip
+    end
   end
 end
