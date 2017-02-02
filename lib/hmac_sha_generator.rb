40e0acc9a57fc8bc9aa6c11f22df696e01fc6695
Sean Eshbaugh <seaneshbaugh@gmail.com>
2017-02-01 23:30:04 -0600

    Rubocop autocorrect the easy stuff.

--- lib/hmac_sha_generator.rb
+++ lib/hmac_sha_generator.rb
@@ -1,11 +1,11 @@
 class BookshoutHmacShaAuth::HmacShaGenerator
-  def self.build_signature(timestamp_str, params_string,application="www")
-    #key = "bookshout_key"
+  def self.build_signature(timestamp_str, params_string, application = 'www')
+    # key = "bookshout_key"
     begin
-      #env_key =YAML.load_file("#{Rails.root}/config/hmac_sha_envs.yml")["hmac_sha_env_key"]
+      # env_key =YAML.load_file("#{Rails.root}/config/hmac_sha_envs.yml")["hmac_sha_env_key"]
       env_key = "BOOKSHOUT_#{application.upcase}_HMAC_KEY"
     rescue
-      env_key = "BOOKSHOUT_HMAC_SHA_LOCAL_KEY"
+      env_key = 'BOOKSHOUT_HMAC_SHA_LOCAL_KEY'
     end
     key = ENV[env_key]
     data = timestamp_str + params_string
--- lib/hmac_shable.rb
+++ lib/hmac_shable.rb
@@ -6,9 +6,9 @@ module BookshoutHmacShaAuth::HmacShable
   extend ::ActiveSupport::Concern
 
   def handle_auth
-    timestamp  = request.headers["X-Bs-Timestamp"]
-    param_list = request.headers["X-Bs-Param-List"]
-    signature  = request.headers["X-Bs-Signature"]
+    timestamp  = request.headers['X-Bs-Timestamp']
+    param_list = request.headers['X-Bs-Param-List']
+    signature  = request.headers['X-Bs-Signature']
 
     unless timestamp.present? && param_list.present? && signature.present?
       # TODO: Just return false instead of calling render. This makes way too many assumptions about how failures should be handled.
@@ -18,30 +18,30 @@ module BookshoutHmacShaAuth::HmacShable
     end
 
     datetime = DateTime.parse(timestamp)
-    param_str   = ""
-    param_list.split(",").each do |param|
-      param_str << (params[param.to_sym].to_s || "")
+    param_str = ''
+    param_list.split(',').each do |param|
+      param_str << (params[param.to_sym].to_s || '')
     end
-    app_name = YAML.load_file("#{Rails.root}/config/hmac_sha_envs.yml")["app_name"]
-    computed_signature = BookshoutHmacShaAuth::HmacShaGenerator.build_signature timestamp,param_str,app_name
-    computed_signature = URI::encode(computed_signature.strip)
+    app_name = YAML.load_file("#{Rails.root}/config/hmac_sha_envs.yml")['app_name']
+    computed_signature = BookshoutHmacShaAuth::HmacShaGenerator.build_signature(timestamp, param_str, app_name)
+    computed_signature = URI.encode(computed_signature.strip)
 
-    #if user_id && timestamp && param_list && signature &&  computed_signature == signature
+    # if user_id && timestamp && param_list && signature &&  computed_signature == signature
     successfull_attempt = signature == computed_signature
     Rails.logger.debug "Access: #{successfull_attempt}" if defined?(Rails)
 
-    if datetime < DateTime.now-25.minute
-      Rails.logger.debug "INVALID TIMESTAMP" if defined?(Rails)
+    if datetime < DateTime.now - 25.minute
+      Rails.logger.debug 'INVALID TIMESTAMP' if defined?(Rails)
 
       # TODO: Again, just return false instead of calling render for the same reason as above.
-      render(json: {:message => "Invalid timestamp. Too far in the past. Request expired."}, :status => 401 )
+      render(json: { message: 'Invalid timestamp. Too far in the past. Request expired.' }, status: 401)
 
       return false
     end
 
     unless successfull_attempt
       # TODO: Again, return false instead of calling render for the same reason as above.
-      render(json: {:message => "Invalid auth credentials."}, :status => 401 )
+      render(json: { message: 'Invalid auth credentials.' }, status: 401)
 
       return false
     end
@@ -51,12 +51,10 @@ module BookshoutHmacShaAuth::HmacShable
 
   # TODO: Remove this. It's apparently relying on the old behavior of throwing an exception when the request headers aren't set. Also, see the note at the top.
   def handle_grape_auth
-    begin
-      handle_auth
-      true
-    rescue Exception => e
-      Rails.logger.error e.to_s if defined?(Rails)
-      false
-    end
+    handle_auth
+    true
+  rescue Exception => e
+    Rails.logger.error e.to_s if defined?(Rails)
+    false
   end
 end
